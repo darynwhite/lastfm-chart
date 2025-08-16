b75132df41a8bc6ec294bd390431a5e1eee62934
Daryn <40304663+darynwhite@users.noreply.github.com>
2025-08-16 11:09:28 -0700

    Update lastfm.py
    Pull DEBUG for release

--- lastfm.py
+++ lastfm.py
@@ -5,7 +5,7 @@ import logging
 
 # Setup logging configuration
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
